d786fa1c277ae6ce0f9be455e8e2c89003ae5f71
wathi <68807000+wathi@users.noreply.github.com>
2026-08-16 20:25:13 +0900

    update info content

--- resources/views/info/content.blade.php
+++ resources/views/info/content.blade.php
@@ -1,4 +1,4 @@
-<x-layout title="ラフラフの見どころ | ラフｘラフデータベース - rough x laugh database -unofficial fans site-">
+<x-layout title="おすすめコンテンツ | ラフｘラフデータベース - rough x laugh database -unofficial fans site-">
     <div class="max-w-8xl mx-auto flex">
       <div class="hidden lg:block basis-1/4">
         <x-sidebar />
@@ -7,6 +7,14 @@
         <h1 class="text-2xl font-bold mb-4">おすすめコンテンツ</h1>
         <p class="mb-8">ラフラフは様々なコンテンツがあります。公式のおすすめは<a class="text-blue-600 link link-hover" href="https://roughlaugh-official.com/news/1774/"  target="_blank">「ラフ×ラフ 推し活スタートガイド」</a >にチェックできます。</p>
         
+        <div class="mb-8">
+            <div class="text-xl font-bold mb-2">自分のキャラを決めよう</div>
+            <p class="mb-1">メンバーの個性を深堀りして、ほぼすべての個人企画はここからスタート。</p>
+            <iframe class="mb-1 w-[360px] h-[240px] md:w-[560px] md:h-[315px]" src="https://www.youtube.com/embed/videoseries?si=_VoWxfPFUNi5xGHQ&amp;list=PLH1WFpP9oMo0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>            <button class="btn btn-outline">
+              <a href="https://youtube.com/playlist?list=PLH1WFpP9oMo0&si=de21d3NbGddCtg2a" target="_blank" class="link link-hover">YouTubeプレイリスト</a>
+            </button>
+        </div>
+
         <div class="text-xl font-bold mb-4">大好評公式Youtube企画</div>
         <div class="mb-4">
             <p class="font-bold text-gray-700 text-lg mb-1">Barもなみん</p>
